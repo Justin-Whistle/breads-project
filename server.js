a7fd78247a70d0f5f49ac982ad6e4b2b421b5f87
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-03-07 21:37:32 -0600

    Custom helper methods

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 const app = express()
 
 // ROUTES
