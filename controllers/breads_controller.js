f5361a4d7a6eb2c70c2fbb185b557fc36b070d56
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-03-09 21:51:53 -0600

    added virtuals/new page not working still

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker.js')
 
-// INDEX page for localhost:3000/breads
+// Display Bakers in Database
+breads.get('/new', (req, res) => {
+  Baker.find()
+      .then(foundBakers => {
+          res.render('new', {
+              bakers: foundBakers
+          })
+    })
+})
+
+// Index:
 breads.get('/', (req, res) => {
-  Bread.find()  
-  .then(foundBreads => {
-    res.render('index',
-    {
-      breads: foundBreads,
-      title: 'Index Page'
-    }
-  )
-  })
+  Baker.find()
+    .then(foundBakers => {
+      Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              bakers: foundBakers,
+              title: 'Index Page'
+          })
+      })
+    })
 })
 
 // CREATE
@@ -29,48 +42,43 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
-// NEW
+// new route
 breads.get('/new', (req, res) => {
-  res.render('new')
+    Baker.find()
+        .then(foundBakers => {
+            res.render('new', {
+                bakers: foundBakers
+            })
+      })
 })
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-  Bread.findById(req.params.id) 
-    .then(foundBread => { 
-      res.render('edit', {
-        bread: foundBread 
-      })
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers 
+            })
+          })
     })
 })
 
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
+      .populate('baker')
       .then(foundBread => {
-        const bakedBy = foundBread.getBakedBy()
-        console.log(bakedBy)
-          res.render('show', {
-              bread: foundBread
-          })
+        res.render('show', {
+            bread: foundBread
+        })
       })
       .catch(err => {
         res.send('404')
       })
 })
-
-// Old SHOW endpoint before connecting MongoDB
-// SHOW
-//breads.get('/:arrayIndex', (req, res) => {
-//  if (Bread[req.params.arrayIndex]) {
-//    res.render('show', { 
-//      bread:Bread[req.params.arrayIndex],
-//      index: req.params.arrayIndex,
-//    })
-//  } else {
-//    res.send('<h1>404 Page Not Found<h1/>')
-//  }
-//})
 
 // UPDATE
 breads.put('/:id', (req, res) => {
@@ -86,7 +94,7 @@ breads.put('/:id', (req, res) => {
     })
 })
 
-// Create a Seed Route for Data for Developers
+// Seed Route 
 breads.get('/data/seed', (req, res) => {
   Bread.insertMany([
     {
